eb04ff1354a9e04c0e5f8e207d28705b07a7a6a0
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-13 04:33:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,6 +63,6 @@ class Student
     sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end
+    end.first
   end
 end
